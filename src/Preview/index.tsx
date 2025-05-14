226c92a03f0233784354072ff7514adcf261154a
Elliot <hack00mind@gmail.com>
2025-05-14 13:43:55 +0800

    feat: use shiki to support more languages

--- src/Preview/index.tsx
+++ src/Preview/index.tsx
@@ -1,61 +1,63 @@
+import rehypeShiki from "@shikijs/rehype";
+import { inlineContent } from "juice";
+import katex from "rehype-katex";
+import stringify from "rehype-stringify";
+import { createEffect, createSignal } from "solid-js";
 import { unified } from "unified";
 import parse from "uniorg-parse";
 import uniorg2rehype from "uniorg-rehype";
-import highlight from "rehype-highlight";
-import stringify from "rehype-stringify";
-import katex from "rehype-katex";
-import { inlineContent } from "juice";
 import linkToCite from "~/utils/cite";
-import { createSignal, createEffect } from "solid-js";
 
 type Props = {
-  org: string;
-  theme: string;
+	org: string;
+	theme: string;
 };
 
 const processor = unified()
-  .use(parse)
-  .use(uniorg2rehype, {
-    imageFilenameExtensions: [
-      "png",
-      "jpeg",
-      "jpg",
-      "gif",
-      "tiff",
-      "tif",
-      "xbm",
-      "xpm",
-      "pbm",
-      "pgm",
-      "ppm",
-      "pnm",
-      "svg",
-      "webp",
-    ],
-  })
-  .use(highlight)
-  .use(katex)
-  .use(linkToCite)
-  .use(stringify);
+	.use(parse)
+	.use(uniorg2rehype, {
+		imageFilenameExtensions: [
+			"png",
+			"jpeg",
+			"jpg",
+			"gif",
+			"tiff",
+			"tif",
+			"xbm",
+			"xpm",
+			"pbm",
+			"pgm",
+			"ppm",
+			"pnm",
+			"svg",
+			"webp",
+		],
+	})
+	.use(rehypeShiki, {
+		theme: "everforest-light",
+	})
+	.use(katex)
+	.use(linkToCite)
+	.use(stringify);
 
 export default function Preview(props: Props) {
-  const [html, setHtml] = createSignal("");
+	const [html, setHtml] = createSignal("");
 
-  createEffect(() => {
-    if (props.org) {
-      processor.process(props.org).then((vfile) => {
-        const htmlStr = vfile.value.toString();
-        const inlined = inlineContent(htmlStr, props.theme, {
-          inlinePseudoElements: true,
-        });
-        setHtml(inlined);
-      });
-    }
-  });
+	createEffect(() => {
+		if (props.org) {
+			processor.process(props.org).then((vfile) => {
+				const htmlStr = vfile.value.toString();
+				const inlined = inlineContent(htmlStr, props.theme, {
+					inlinePseudoElements: true,
+				});
+				setHtml(inlined);
+			});
+		}
+	});
 
-  return (
-    <div class="h-full min-w-[375px] text-sm box-border outline-none shadow-md break-all overflow-auto">
-      <section id="org-output" innerHTML={html()} />
-    </div>
-  );
+	return (
+		<div class="h-full min-w-[375px] text-sm box-border outline-none shadow-md break-all overflow-auto">
+			<section id="org-output" innerHTML={html()} />
+		</div>
+	);
 }
